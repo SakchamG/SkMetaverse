5c843fb728f882cb7301953998babab688d93f09
Sk <tavishmy@gmail.com>
2026-03-18 00:42:29 +0530

    SEO friendly robot and many more fixed

--- next.config.ts
+++ next.config.ts
@@ -17,12 +17,6 @@ const nextConfig: NextConfig = {
         destination: "https://skmetaverse.space/:path*",
         permanent: true,
       },
-      {
-        source: "/:path*",
-        has: [{ type: "header", key: "x-forwarded-proto", value: "http" }],
-        destination: "https://skmetaverse.space/:path*",
-        permanent: true,
-      },
     ];
   },
 };
