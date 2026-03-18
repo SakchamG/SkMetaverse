5a7c910057550923d2d57a136342e05f0f295285
Sk <tavishmy@gmail.com>
2026-03-18 11:37:19 +0530

    fixed up the seo

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.skmetaverse.space" }],
+        destination: "https://skmetaverse.space/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
